f8d8f067ce9c72f830da7cd91b677f7ed62e5f7d
ciro bizelli <bzll926@gmail.com>
2025-09-03 23:28:16 -0400

    feat: implement albums filter and photos list components; enhance photo details page with navigation and loading states

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,13 +1,20 @@
 import Container from "../components/container";
-import PhotoWidget from "../contexts/photos/components/photo-widget";
-import type { Photo } from "../contexts/photos/models/photo";
+import AlbumsFilter from "../contexts/albums/components/albums-filter";
+import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
     <Container>
-      <div className="grid grid-cols-5 gap-9">
-        <PhotoWidget
-          photo={{
+      <AlbumsFilter
+        albums={[
+          { id: "1", title: "Album 1" },
+          { id: "2", title: "Album 2" },
+        ]}
+        className="mb-9"
+      />
+      <PhotosList
+        photos={[
+          {
             id: "1",
             title: "Photo 1",
             imageId: "portrait-tower.png",
@@ -25,33 +32,9 @@ export default function PageHome() {
                 title: "Album 3",
               },
             ],
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: "1",
-            title: "Photo 1",
-            imageId: "portrait-tower.png",
-            albums: [
-              {
-                id: "1",
-                title: "Album 1",
-              },
-              {
-                id: "2",
-                title: "Album 2",
-              },
-              {
-                id: "3",
-                title: "Album 3",
-              },
-            ],
-          }}
-        />
-        <PhotoWidget photo={{} as Photo} loading />
-        <PhotoWidget photo={{} as Photo} loading />
-        <PhotoWidget photo={{} as Photo} loading />
-      </div>
+          },
+        ]}
+      />
     </Container>
   );
 }
--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,15 +1,46 @@
 import { useParams } from "react-router";
+import Container from "../components/container";
 import Text from "../components/text";
-import Divider from "../components/divider";
+import type { Photo } from "../contexts/photos/models/photo";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 
 export default function PagePhotoDetails() {
   const { id } = useParams();
 
+  // Apenas para teste de mock
+  const isLoadingPhoto = false;
+  const photo = {
+    id: "1",
+    title: "Photo 1",
+    imageId: "portrait-tower.png",
+    albums: [
+      {
+        id: "1",
+        title: "Album 1",
+      },
+      {
+        id: "2",
+        title: "Album 2",
+      },
+      {
+        id: "3",
+        title: "Album 3",
+      },
+    ],
+  } as Photo;
+
   return (
-    <>
-      <Text variant="heading-medium">Página detalhe da Foto</Text>
-      <Divider />
-      <Text variant="label-medium">ID da Foto: {id}</Text>
-    </>
+    <Container>
+      <header className="flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+          <Text variant="heading-large">{photo?.title}</Text>
+        ) : (
+          <Skeleton className="w-48 h-8" />
+        )}
+
+        <PhotosNavigator loading={isLoadingPhoto} />
+      </header>
+    </Container>
   );
 }
